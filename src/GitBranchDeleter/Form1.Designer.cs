11e6cb027dfa6c02e1fd40c4e3a7ae201c3666a3
Wade Gausden <wade.gausden@gmail.com>
2016-04-23 12:08:27 +1200

    Initial code commit.

---
+++ src/GitBranchDeleter/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace GitBranchDeleter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFolderPath = new System.Windows.Forms.TextBox();
+            this.buttonBrowser = new System.Windows.Forms.Button();
+            this.buttonFetchBranches = new System.Windows.Forms.Button();
+            this.treeViewBranches = new System.Windows.Forms.TreeView();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.listBoxUpdates = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxFolderPath
+            // 
+            this.textBoxFolderPath.Location = new System.Drawing.Point(12, 12);
+            this.textBoxFolderPath.Name = "textBoxFolderPath";
+            this.textBoxFolderPath.Size = new System.Drawing.Size(284, 20);
+            this.textBoxFolderPath.TabIndex = 0;
+            // 
+            // buttonBrowser
+            // 
+            this.buttonBrowser.Location = new System.Drawing.Point(302, 10);
+            this.buttonBrowser.Name = "buttonBrowser";
+            this.buttonBrowser.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowser.TabIndex = 1;
+            this.buttonBrowser.Text = "Browse";
+            this.buttonBrowser.UseVisualStyleBackColor = true;
+            this.buttonBrowser.Click += new System.EventHandler(this.buttonBrowser_Click);
+            // 
+            // buttonFetchBranches
+            // 
+            this.buttonFetchBranches.Location = new System.Drawing.Point(139, 38);
+            this.buttonFetchBranches.Name = "buttonFetchBranches";
+            this.buttonFetchBranches.Size = new System.Drawing.Size(113, 23);
+            this.buttonFetchBranches.TabIndex = 2;
+            this.buttonFetchBranches.Text = "Fetch Branches";
+            this.buttonFetchBranches.UseVisualStyleBackColor = true;
+            this.buttonFetchBranches.Click += new System.EventHandler(this.buttonFetchBranches_Click);
+            // 
+            // treeViewBranches
+            // 
+            this.treeViewBranches.CheckBoxes = true;
+            this.treeViewBranches.Location = new System.Drawing.Point(12, 67);
+            this.treeViewBranches.Name = "treeViewBranches";
+            this.treeViewBranches.Size = new System.Drawing.Size(365, 234);
+            this.treeViewBranches.TabIndex = 3;
+            this.treeViewBranches.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeViewBranches_AfterCheck);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(137, 307);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(117, 23);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "Delete Branches";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // listBoxUpdates
+            // 
+            this.listBoxUpdates.FormattingEnabled = true;
+            this.listBoxUpdates.Location = new System.Drawing.Point(12, 336);
+            this.listBoxUpdates.Name = "listBoxUpdates";
+            this.listBoxUpdates.Size = new System.Drawing.Size(365, 160);
+            this.listBoxUpdates.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 516);
+            this.Controls.Add(this.listBoxUpdates);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.treeViewBranches);
+            this.Controls.Add(this.buttonFetchBranches);
+            this.Controls.Add(this.buttonBrowser);
+            this.Controls.Add(this.textBoxFolderPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Git Branch Deleter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFolderPath;
+        private System.Windows.Forms.Button buttonBrowser;
+        private System.Windows.Forms.Button buttonFetchBranches;
+        private System.Windows.Forms.TreeView treeViewBranches;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.ListBox listBoxUpdates;
+    }
+}
+
---
+++ src/GitBranchDeleter/Form1.cs
@@ -0,0 +1,171 @@
+﻿using NGit;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitBranchDeleter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowser_Click(object sender, EventArgs e)
+        {
+            var folderSelector = new FolderBrowserDialog();
+            folderSelector.Description = "Select your GIT project root folder";
+            if (folderSelector.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(folderSelector.SelectedPath))
+            {
+                textBoxFolderPath.Text = folderSelector.SelectedPath;
+            }
+        }
+
+        private async void buttonFetchBranches_Click(object sender, EventArgs e)
+        {
+            treeViewBranches.BeginUpdate();
+            treeViewBranches.Nodes.Clear();
+            await FetchBranchesAndFillTree();
+            treeViewBranches.EndUpdate();
+
+        }
+
+        private async Task FetchBranchesAndFillTree()
+        {
+            var gitRepository = NGit.Api.Git.Open(textBoxFolderPath.Text).GetRepository();
+
+            foreach (var branch in gitRepository.GetAllRefs())
+            { 
+                //Not really needed. I was doing something heavy here before that actually did await correctly....
+                await Task.Yield();
+
+                if (branch.Key.StartsWith("refs/remotes/"))
+                    continue;
+
+                var splitPath = branch.Key.Split('/');
+                TreeNode previousNode = null;
+                for (int level = 0; level < splitPath.Count(); level++)
+                {
+                    TreeNode newNode = null;
+                    if (previousNode != null)
+                    {
+                        newNode = previousNode.Nodes.Find(splitPath[level], false).FirstOrDefault();
+                        if (newNode == null)
+                        {
+                            newNode = previousNode.Nodes.Add(splitPath[level], splitPath[level]);
+                        }
+                    }
+                    else
+                    {
+                        newNode = treeViewBranches.Nodes.Find(splitPath[level], true).FirstOrDefault();
+                        if (newNode == null)
+                        {
+                            newNode = treeViewBranches.Nodes.Add(splitPath[level], splitPath[level]);
+                        }
+                    }
+
+                    previousNode = newNode;
+                }
+            }
+
+            gitRepository.Close();
+        }
+
+        private void treeViewBranches_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            //Passes down the check request to check all children. 
+            if (e.Action != TreeViewAction.Unknown)
+            {
+                if (e.Node.Nodes.Count > 0)
+                {
+                    foreach (TreeNode node in e.Node.Nodes)
+                    {
+                        this.CheckAllChildNodes(e.Node, e.Node.Checked);
+                    }
+                }
+            }
+
+            //Passes UP the check request to say that the tick should no longer be there (Since it hasn't selected all children. 
+            if(e.Node.Checked == false && e.Node.Parent != null)
+            {
+                e.Node.Parent.Checked = false;
+            }
+        }
+
+        private void CheckAllChildNodes(TreeNode treeNode, bool nodeChecked)
+        {
+            foreach (TreeNode node in treeNode.Nodes)
+            {
+                node.Checked = nodeChecked;
+                if (node.Nodes.Count > 0)
+                {
+                    this.CheckAllChildNodes(node, nodeChecked);
+                }
+            }
+        }
+
+        private async void buttonDelete_Click(object sender, EventArgs e)
+        {
+            foreach (var node in treeViewBranches.Nodes.Cast<TreeNode>())
+            {
+                var nodeList = GetTreeNodeDescendants(node, new List<TreeNode>());
+                foreach (var branchNode in nodeList)
+                {
+                    if (branchNode.Checked)
+                    {
+                        var branchName = GetFullGitPathFromTreeNode(branchNode, string.Empty);
+                        await Task.Yield();
+                        NGit.Api.Git.Open(textBoxFolderPath.Text)
+                            .BranchDelete()
+                            .SetBranchNames(branchName)
+                            .SetForce(true)
+                            .Call();
+
+                        UpdateStatus("Successfully deleted the branch " + branchName);
+                    }
+                }
+            }
+        }
+
+        private string GetFullGitPathFromTreeNode(TreeNode node, string childPath)
+        {
+            if (node.Parent != null)
+                return GetFullGitPathFromTreeNode(node.Parent, "/" + node.Text + childPath);
+            else
+                return node.Text + childPath;
+        }
+
+        private IEnumerable<TreeNode> GetTreeNodeDescendants(TreeNode node, List<TreeNode> currentList)
+        {
+            if (node.Nodes.Count == 0)
+            {
+                currentList.Add(node);
+            }
+            else
+            {
+                foreach(var childNode in node.Nodes.Cast<TreeNode>())
+                {
+                    GetTreeNodeDescendants(childNode, currentList);
+                }
+            }
+
+            return currentList;
+        }
+
+        private void UpdateStatus(string status)
+        {
+            var scrollBox = false;
+            if (listBoxUpdates.SelectedIndex == listBoxUpdates.Items.Count - 1)
+                scrollBox = true;
+
+            listBoxUpdates.Items.Add(status);
+
+            if (scrollBox)
+                listBoxUpdates.SelectedIndex = listBoxUpdates.Items.Count - 1;
+        }
+    }
+}
